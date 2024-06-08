b84898ef0ee3761c2812ec86b48c4d266fa48538
PatrickTLuu <pluu079@uottawa.ca>
2024-06-08 19:17:32 -0400

    Add option to add another person to an appointment (Persona 2)

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,9 +1,12 @@
 const bookingForm = document.getElementById("booking-form");
-const dateDropdown = document.getElementById("date");
+const barberDropdown = document.getElementById("barber")
+const dateInput = document.getElementById("date");
 const timeDropdown = document.getElementById("time");
+const frequencyDropdown = document.getElementById("frequency");
+const addPersonBtn = document.getElementById("add-person-btn")
 
 function setTimeOptions() {
-    const day = new Date(dateDropdown.value);
+    const day = new Date(dateInput.value);
 
     if ((day.getDay() + 1) % 7 == 0) { //Sunday
         var option = new Option();
@@ -29,30 +32,67 @@ function setTimeOptions() {
 function removeTimeOptions() {
     const option = new Option();
     option.innerText = "-------------------";
-    option.id = "time-header";
     option.value = "";
     timeDropdown.innerHTML = "";
     timeDropdown.appendChild(option);
 }
 
-function submitForm(event) {
+function addPerson() {
+    const options = barberDropdown.getElementsByTagName("option");
+    const barber = localStorage.getItem("barber");
+    const date = localStorage.getItem("date");
+    const time = localStorage.getItem("time");
+    const frequency = localStorage.getItem("frequency");
+
+    for (let i = 0; i < options.length; i++) {
+        if (options.item(i).innerText == barber) {
+            options.item(i).remove();
+            break;
+        }
+    }
+
+    dateInput.value = date;
+    dateInput.innerHTML = "value=\"" + date + "\"";
+    dateInput.removeEventListener("click", removeTimeOptions);
+
+    const timeDropdownOption = new Option();
+    timeDropdownOption.innerText = time;
+
+    timeDropdown.removeEventListener("click", setTimeOptions);
+    timeDropdown.innerHTML = "";
+    timeDropdown.appendChild(timeDropdownOption);
+
+    const frequencyDropdownOption = new Option();
+    frequencyDropdownOption.innerText = frequency;
+
+    frequencyDropdown.innerHTML = "";
+    frequencyDropdown.appendChild(frequencyDropdownOption);
+}
+
+function storeFormData(event) {
     var form = new FormData(event.target);
     form.forEach((value, key) => {
+        if (localStorage.getItem(key) != null) {
+            key += "1";
+        }
+
         localStorage.setItem(key, value);
     })
-    window.location.assign("/confirmation.html");
 }
 
 bookingForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    submitForm(event);
+    storeFormData(event);
+    bookingForm.reset();
+
+    if (event.submitter.id == "add-person-btn") {
+        localStorage.setItem("2-people", "true");
+        addPerson();
+    } else {
+        window.location.assign("/confirmation.html");
+    }
 });
 
-dateDropdown.addEventListener("click", (event) => {
-    removeTimeOptions();
-})
+dateInput.addEventListener("click", removeTimeOptions);
 
-timeDropdown.addEventListener("click", (event) => {
-    event.preventDefault();
-    setTimeOptions();
-});
+timeDropdown.addEventListener("click", setTimeOptions);
